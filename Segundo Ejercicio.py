7c04884ef8bb9645c72b670da460b8a3c81559f3
Daniel A. Rodas Flores <daniel.rodas123@gmail.com>
2019-09-21 18:29:33 +0200

    Create Segundo Ejercicio.py

---
+++ Segundo Ejercicio.py
@@ -0,0 +1,50 @@
+#Primer ejercicio cuadrado:
+    # Author: Daniel A. Rodas
+    # Date: 18/09/2019
+    # walking around de world with a definition
+    def walk():
+        move()
+        move()
+        move()
+        move()
+        move()
+        move()
+        move()
+        move()
+        move()
+    # Main program
+
+    walk()
+    turn_left()
+    walk()
+    turn_left()
+    walk()
+    turn_left()
+    walk()
+
+#Segundo Ejercicio escaleras:
+    # Author: Daniel A. Rodas
+    # Date: 18/09/2019
+    # walking around de world with a definition
+
+    def turnrigth():
+        turn_left()
+        turn_left()
+        turn_left()
+
+    def ladder():
+        move()
+        turn_left()
+        move()
+        turnrigth()
+    # Main program
+
+    ladder()
+    ladder()
+    ladder()
+    ladder()
+    ladder()
+    ladder()
+    ladder()
+    ladder()
+    ladder()
